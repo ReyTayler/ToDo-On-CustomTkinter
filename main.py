2f1a250cd91f13dfaf12bc21c99c0071ff9f4a7d
Ilya <ilyapavlov200311@gmail.com>
2025-03-04 20:49:00 +0300

    modified:   .idea/TODO List.iml 	modified:   config/list_do.json 	modified:   main.py

--- main.py
+++ main.py
@@ -3,4 +3,3 @@ from src.view import ToDoListApp
 if __name__ == "__main__":
     app = ToDoListApp()  # Создаём экземпляр класса нашего окна
     app.mainloop()  # Запускаем жизненный цикл для этого окна
-
